09f122a5bc44bc4ed1003333f69d12b685d4ee47
milan kumar15 <milan.kumar_da17@gla.ac.in>
2017-10-29 08:34:46 +0530

    my last commit

--- milan.c
+++ milan.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 void main()
 {
-printf("HELLO THIS IS MY GITHUB ASSIGNMENT");
+int a,b,c;
+printf("Enter Two numbers:");
+scanf("%d%d",&a,&b);
+c=a;
+a=b;
+b=c;
+printf("The Two numbers after swapping are:%d%d",a,b);
 }
